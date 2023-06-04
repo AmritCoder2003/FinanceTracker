6f128de0c9cfc0fbf4c143fef2350ad0b44316d4
Amrit Chattopadhyay <67870234+AMRIT456@users.noreply.github.com>
2023-06-04 21:48:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 // let transactions = [];
 const balanceAmount = document.getElementById("balanceAmount");
+const incomeAmount = document.getElementById("incomeAmount");
+const expenseAmount = document.getElementById("expenseAmount");
 const transactionForm = document.getElementById("transactionForm");
 const descriptionInput = document.getElementById("descriptionInput");
 const amountInput = document.getElementById("amountInput");
@@ -8,18 +10,35 @@ const typeSelect = document.getElementById("typeSelect");
 const transactionList = document.getElementById("transactionList");
 const noTransactionMessage = document.getElementById("noTransactionMessage");
 let transactions = [];
+let incomes=0;
+let expenses=0;
 
 // Check if transactions data exists in Local Storage
 if (localStorage.getItem("transactions")) {
     transactions = JSON.parse(localStorage.getItem("transactions"));
 }
+
 // Function to update balance
+
 function updateBalance() {
     const balance = transactions.reduce((acc, transaction) => {
-        return transaction.type === "income" ? acc + transaction.amount : acc - transaction.amount;
+      return transaction.type === "income" ? acc + transaction.amount : acc - transaction.amount;
     }, 0);
+  
+    totalIncome = transactions.reduce((acc, transaction) => {
+      return transaction.type === "income" ? acc + transaction.amount : acc;
+    }, 0);
+  
+    totalExpense = transactions.reduce((acc, transaction) => {
+      return transaction.type === "expense" ? acc + transaction.amount : acc;
+    }, 0);
+  
     balanceAmount.textContent = balance.toFixed(2);
+    incomeAmount.textContent = totalIncome.toFixed(2);
+    expenseAmount.textContent = totalExpense.toFixed(2);
+
 }
+
 // Function to add a transaction
 function addTransaction(event) {
     event.preventDefault();
@@ -33,11 +52,19 @@ function addTransaction(event) {
         amount,
         type
     };
-
+    
+    
     transactions.push(transaction);
+    if(type==="income"){
+        totalIncome+=amount;
+    }
+    else if(type==="expense"){
+        totalExpense+=amount;
+    }
     updateTransactions();
     updateBalance();
     saveTransactions();
+    alert(`Transaction "${transaction.description}" added successfully!`);
 
     descriptionInput.value = "";
     amountInput.value = "";
@@ -46,16 +73,23 @@ function addTransaction(event) {
 
 // Function to delete a transaction
 function deleteTransaction(index) {
-    transactions.splice(index, 1);
+    const deletedTransaction = transactions.splice(index, 1);
+    if (deletedTransaction.type === "income") {
+        totalIncome -= deletedTransaction.amount;
+      } else if (deletedTransaction.type === "expense") {
+        totalExpense -= deletedTransaction.amount;
+      }
+      alert(`Transaction deleted successfully!`);
     updateTransactions();
     updateBalance();
     saveTransactions();
+    
 }
 
 // Function to save transactions to Local Storage
 function saveTransactions() {
     localStorage.setItem("transactions", JSON.stringify(transactions));
-}
+    }
 
 // Function to load transactions from Local Storage
 function loadTransactions() {
@@ -80,7 +114,7 @@ function updateTransactions() {
             const listItem = document.createElement("li");
             listItem.innerHTML = `
                 <span>${transaction.description}</span>
-                <span class="${transaction.type}">${transaction.type === "income" ? "+" : "-"}${transaction.amount}</span>
+                <span class="amount ${transaction.type}">${transaction.type === "income" ? "+" : "-"}${transaction.amount.toFixed(2)}</span>
                 <div class="transaction-actions">
                     <button onclick="editTransaction(${index})">Edit</button>
                     <button onclick="deleteTransaction(${index})">Delete</button>
@@ -98,15 +132,26 @@ function editTransaction(index) {
     const updatedType = prompt("Enter the updated type (income or expense):", transaction.type);
 
     if (updatedDescription && !isNaN(updatedAmount) && (updatedType === "income" || updatedType === "expense")) {
-        transactions[index] = {
+        const updatedTransaction = {
             description: updatedDescription,
             amount: updatedAmount,
             type: updatedType
-        };
-
+          };
+      
+          if (transaction.type === "income") {
+            totalIncome -= transaction.amount;
+            totalIncome += updatedAmount;
+          } else if (transaction.type === "expense") {
+            totalExpense -= transaction.amount;
+            totalExpense += updatedAmount;
+          }
+      
+        transactions[index] = updatedTransaction;
+        alert(`Transaction "${updatedTransaction.description}" updated successfully!`);
         updateTransactions();
         updateBalance();
         saveTransactions();
+        
     } else {
         alert("Invalid input! Please try again.");
     }
@@ -114,6 +159,19 @@ function editTransaction(index) {
 
 // Event listener for adding a transaction
 transactionForm.addEventListener("submit", addTransaction);
+function updateDateTime() {
+  const currentDateTime = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  const formattedDateTime = currentDateTime.toLocaleDateString('en-US', options);
+  document.getElementById('currentDateTime').textContent = formattedDateTime;
+}
+
+// Update the date and time every second
+setInterval(updateDateTime, 1000);
+
+// Initial update
+updateDateTime();
+
 
 // Load transactions on page load
 loadTransactions();
